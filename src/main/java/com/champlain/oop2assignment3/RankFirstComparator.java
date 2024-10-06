17f6ad73587b95b029e7f39c61905af64d5bc079
Tarin <mohammadtarin.wahidi@crcmail.net>
2024-10-06 12:27:03 -0400

    Implemented functional sorting by Rank first

--- src/main/java/com/champlain/oop2assignment3/RankFirstComparator.java
+++ src/main/java/com/champlain/oop2assignment3/RankFirstComparator.java
@@ -2,8 +2,16 @@ package com.champlain.oop2assignment3;
 
 import java.util.Comparator;
 
-public class RankFirstComparator implements Comparator {
-    public int compare(Object o1, Object o2) {
-        return 0;
+public class RankFirstComparator implements Comparator<Card> {
+
+    @Override
+    public int compare(Card firstCard, Card secondCard) {
+
+        int comparison = firstCard.getRank().compareTo(secondCard.getRank());
+
+        if (comparison == 0)
+            return firstCard.getSuit().compareTo(secondCard.getSuit());
+
+        return comparison;
     }
 }
