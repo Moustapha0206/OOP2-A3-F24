a72147dbb07b92cd4454f3f4b2fc84f4a370910d
Tarin <mohammadtarin.wahidi@crcmail.net>
2024-10-06 11:39:53 -0400

    Added the RankFirstComparator.java class

---
+++ src/main/java/com/champlain/oop2assignment3/RankFirstComparator.java
@@ -0,0 +1,9 @@
+package com.champlain.oop2assignment3;
+
+import java.util.Comparator;
+
+public class RankFirstComparator implements Comparator {
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
